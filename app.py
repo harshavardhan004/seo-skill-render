fa0aafb3876b6cc0a262bad4a2cc87879998b5c7
harshavardhan004 <147902989+harshavardhan004@users.noreply.github.com>
2026-04-20 20:16:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, send_file
 import subprocess
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='Templates')
 
 
 @app.route("/")
